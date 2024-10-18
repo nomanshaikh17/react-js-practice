b9e051dad218e71ff4f8102e8cc076b52cdcd160
nomanshaikh17 <noman.shaikh49@yahoo.com>
2024-10-18 23:27:17 +0500

    conditinal rendring

--- 19-map/src/App.jsx
+++ 19-map/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 
 function App() {
-  let itemArr = ["item1", "item2", "item3", "item4", "item5", "item6"];
+  // let itemArr = ["item1", "item2", "item3", "item4", "item5", "item6"];
+  let itemArr = [];
   return (
     <>
       <h1>map</h1>
+      {itemArr.length==0 ? 'item not found' : null}
       <ul>
         {itemArr.map((item) => (
-          <li>{item}</li>
+          <li key={item}>{item}</li>
         ))}
       </ul>
     </>
