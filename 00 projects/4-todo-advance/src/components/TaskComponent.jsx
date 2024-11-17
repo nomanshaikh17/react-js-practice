7b69fddb508321278e331be9ce22330f1fd4c520
nomanshaikh17 <noman.shaikh49@yahoo.com>
2024-11-17 14:58:46 +0500

    00 project 4-advance todo add checkbox design and task row design updated

--- 00 projects/4-todo-advance/src/components/TaskComponent.jsx
+++ 00 projects/4-todo-advance/src/components/TaskComponent.jsx
@@ -1,16 +1,30 @@
 import css from "../css/TaskComponent.module.css";
+import { FaStar } from "react-icons/fa6";
+import { RiDeleteBinLine } from "react-icons/ri";
+
 
 const TaskComponent = () => {
   return (
     <div className={css["task-main-container"]}>
       <div className={css["inner-container"]}>
-        <div className={css['task-list-main-div']}>
-          <div className={css['heading']}>Tasks</div>
+        <div className={css["task-list-main-div"]}>
+          <div className={css["heading"]}>Tasks</div>
           <div className="task-list-div">
-            <div className={css['task']}>Task 1</div>
+            <div className={css["task"]}>
+              <div className={css["round"]}>
+                <input type="checkbox" checked onChange={()=>{}} />
+                <label htmlFor="checkbox"></label>
+              </div>
+              <span className={css["task-text"]}>Task 1</span>
+              <span className={css["task-star"]}><FaStar /></span>
+              <span className={css["task-bin"]}><RiDeleteBinLine />
+              </span>
+            </div>
           </div>
         </div>
-        <div className={css['bottom-container']}><button className={css['add-button']}>Add</button></div>
+        <div className={css["bottom-container"]}>
+          <button className={css["add-button"]}>Add</button>
+        </div>
       </div>
     </div>
   );
